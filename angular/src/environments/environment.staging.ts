26cae01a8462d52008e54fbef72934697c738100
Nathan <nathangrobler@gmail.com>
2024-04-23 20:44:49 +0200

    Revert "point staging frontend to local backend for debugging"
    This reverts commit d93370255b4d6281a61a3ce1d11b4fe90a309879.

--- angular/src/environments/environment.staging.ts
+++ angular/src/environments/environment.staging.ts
@@ -3,23 +3,23 @@ import { Environment } from '@abp/ng.core';
 const baseUrl = 'https://staging.oakell.com/';
 
 export const environment = {
-  production: false,
+  production: true,
   application: {
     baseUrl,
     name: 'Oakell',
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'http://localhost:44380/',
+    issuer: 'https://api.staging.oakell.com/',
     redirectUri: baseUrl,
     clientId: 'Oakell_App',
     responseType: 'code',
     scope: 'offline_access Oakell',
-    requireHttps: false,
+    requireHttps: false
   },
   apis: {
     default: {
-      url: 'http://localhost:44380',
+      url: 'https://api.staging.oakell.com/',
       rootNamespace: 'Oakell',
     },
   },
