d93370255b4d6281a61a3ce1d11b4fe90a309879
Nathan <nathangrobler@gmail.com>
2024-04-23 20:33:06 +0200

    point staging frontend to local backend for debugging

--- angular/src/environments/environment.staging.ts
+++ angular/src/environments/environment.staging.ts
@@ -3,23 +3,23 @@ import { Environment } from '@abp/ng.core';
 const baseUrl = 'https://staging.oakell.com/';
 
 export const environment = {
-  production: true,
+  production: false,
   application: {
     baseUrl,
     name: 'Oakell',
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://api.staging.oakell.com/',
+    issuer: 'http://localhost:44380/',
     redirectUri: baseUrl,
     clientId: 'Oakell_App',
     responseType: 'code',
     scope: 'offline_access Oakell',
-    requireHttps: false
+    requireHttps: false,
   },
   apis: {
     default: {
-      url: 'https://api.staging.oakell.com/',
+      url: 'http://localhost:44380',
       rootNamespace: 'Oakell',
     },
   },
