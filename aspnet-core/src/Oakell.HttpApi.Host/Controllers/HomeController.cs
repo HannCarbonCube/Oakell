7e0b1886f1075e9e23d69e1c6603b98a1000fa9e
Nathan <nathangrobler@gmail.com>
2024-05-13 21:51:33 +0200

    chore(auth): home controller as well

--- aspnet-core/src/Oakell.HttpApi.Host/Controllers/HomeController.cs
+++ aspnet-core/src/Oakell.HttpApi.Host/Controllers/HomeController.cs
@@ -7,11 +7,7 @@ public class HomeController : AbpController
 {
     public ActionResult Index()
     {
-        // If you don't want to redirect to Swagger by default, comment out or remove the following line.
-        // return Redirect("~/swagger");
-        
-        // Uncomment the following line if you want to return a view or another form of response.
-        return Content("Welcome to the Oakell API!");
+        return Redirect("~/swagger");
+        // return View();
     }
 }
-
