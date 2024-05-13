1025cc12156debed3ca90a36dfecb4e42464c34a
Nathan <nathangrobler@gmail.com>
2024-05-13 21:12:47 +0200

    chore(auth): redirect to base

--- aspnet-core/src/Oakell.HttpApi.Host/Controllers/HomeController.cs
+++ aspnet-core/src/Oakell.HttpApi.Host/Controllers/HomeController.cs
@@ -7,7 +7,11 @@ public class HomeController : AbpController
 {
     public ActionResult Index()
     {
-        return Redirect("~/swagger");
-        // return View();
+        // If you don't want to redirect to Swagger by default, comment out or remove the following line.
+        // return Redirect("~/swagger");
+        
+        // Uncomment the following line if you want to return a view or another form of response.
+        return Redirect("/");
     }
 }
+
