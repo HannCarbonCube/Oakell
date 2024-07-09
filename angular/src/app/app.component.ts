a8ac02732d1bf0080f94eb511c775b6c0117f441
Hannelie <hannelie@carboncube.co.za>
2024-07-09 13:31:47 +0200

    feature sc-49 add space

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ReplaceableComponentsService } from '@abp/ng.core';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
